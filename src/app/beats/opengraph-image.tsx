8e80a88c985b4ad6d0b5a7415ea164db60788b09
sneakzandbeatzllc <sneakzandbeatzllc@gmail.com>
2026-07-28 10:33:51 -0700

    Companion articles: PHRHX Show topics (2026-07-28)

--- src/app/beats/opengraph-image.tsx
+++ src/app/beats/opengraph-image.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { ImageResponse } from "next/og";
 
 export const runtime = "nodejs";
--- src/app/the-lane/[slug]/opengraph-image.tsx
+++ src/app/the-lane/[slug]/opengraph-image.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { ImageResponse } from "next/og";
 import { getEssay } from "@/data/lane-essays";
 
